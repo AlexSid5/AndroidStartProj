b8b85fd72b0f92201f55b4640e08b29ea762c90b
AlexSid5 <evrodom68@yandex.ru>
2023-03-16 17:07:01 +0300

    Added Java screen configuration example

--- app/src/main/java/ru/synergy/androidstartproj/MainActivity.java
+++ app/src/main/java/ru/synergy/androidstartproj/MainActivity.java
@@ -3,12 +3,23 @@ package ru.synergy.androidstartproj;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+      //setContentView(R.layout.activity_main);
+
+        TextView textView = new TextView(this);
+
+        textView.setText("Hello Sanya");
+        textView.setText("Sanya you Stupid");
+        textView.setTextSize(22);
+
+        setContentView(textView);
+
+
     }
 }
