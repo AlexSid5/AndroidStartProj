2376c5693743a3df4e64b15bed22bc7bbe1c8d56
AlexSid5 <evrodom68@yandex.ru>
2023-03-16 23:12:41 +0300

    TextView textView Hello JAVA

---
+++ app/src/main/java/ru/synergy/androidstartproj/MainActivity.java
@@ -0,0 +1,41 @@
+package ru.synergy.androidstartproj;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_layout);
+
+
+        TextView textView = (TextView) findViewById(R.id.header);
+
+        textView.setText("*HELLO FROM JAVA*");
+
+ //       ConstraintLayout constraintLayout = new ConstraintLayout(this);
+ //       TextView textView = new TextView(this);//
+//          textView.setText("!!!Hello Android!!!");
+//          textView.setTextSize(26);
+//
+//        ConstraintLayout.LayoutParams layoutParams = new ConstraintLayout.LayoutParams(
+//                ConstraintLayout.LayoutParams.WRAP_CONTENT,
+//                ConstraintLayout.LayoutParams.WRAP_CONTENT);
+//       layoutParams.leftToLeft = ConstraintLayout.LayoutParams.PARENT_ID;
+//        layoutParams.topToTop = ConstraintLayout.LayoutParams.PARENT_ID;
+//        layoutParams.rightToRight = ConstraintLayout.LayoutParams.PARENT_ID;
+//
+//       textView.setLayoutParams(layoutParams);
+//
+//        constraintLayout.addView(textView);
+//
+//        setContentView(constraintLayout);
+
+
+    }
+}
